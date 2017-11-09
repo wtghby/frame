5c27741f8e01c09b5601f7698aeb882e41e5bfc7
wtghby <wtghbyhui1@163.com>
2017-11-09 15:36:33 +0800

    添加butterknife

--- app/src/main/java/com/conny/frame/MainActivity.java
+++ app/src/main/java/com/conny/frame/MainActivity.java
@@ -2,12 +2,35 @@ package com.conny.frame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.conny.frame.material.BaseActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.text)
+    TextView mText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void initLayout() {
+        addView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void initData() {
+        mText.setText("hhhhhhhhhhhhh");
+    }
+
+    @OnClick({R.id.click})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.click:
+                mText.setText("Click");
+                break;
+        }
     }
 }
--- app/src/main/java/com/conny/frame/material/BaseActivity.java
+++ app/src/main/java/com/conny/frame/material/BaseActivity.java
@@ -1,9 +1,50 @@
 package com.conny.frame.material;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.conny.frame.R;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Desc:
  * Created by zhanghui on 2017/11/9.
  */
 
-public class BaseActivity {
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private FrameLayout mContent;
+    private Unbinder mBinder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base);
+        mContent = (FrameLayout) findViewById(R.id.content);
+        initLayout();
+        mBinder = ButterKnife.bind(this);
+        initData();
+    }
+
+    protected abstract void initLayout();
+
+    protected abstract void initData();
+
+    protected void addView(int layoutId) {
+        View view = LayoutInflater.from(this).inflate(layoutId, null);
+        mContent.addView(view);
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mBinder != null) {
+            mBinder.unbind();
+        }
+        super.onDestroy();
+    }
 }
