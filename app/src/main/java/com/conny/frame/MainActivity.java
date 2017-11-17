02a8b593374be6bfee2dbe07011ff74dbb20cf62
wtghby <wtghbyhui1@163.com>
2017-11-17 17:06:38 +0800

    添加SlidingMenu

--- app/src/main/java/com/conny/frame/MainActivity.java
+++ app/src/main/java/com/conny/frame/MainActivity.java
@@ -1,6 +1,7 @@
 package com.conny.frame;
 
 import android.content.Intent;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 import com.bumptech.glide.Glide;
 import com.conny.frame.material.BaseActivity;
 import com.conny.frame.material.dialog.CommonDialog;
+import com.conny.frame.material.slidingmenu.lib.SlidingMenu;
 import com.conny.frame.test.FrameApi;
 import com.conny.frame.test.IpBean;
 import com.conny.frame.test.PullActivity;
@@ -25,12 +27,15 @@ public class MainActivity extends BaseActivity {
     @BindView(R.id.image)
     ImageView mImage;
 
+    private SlidingMenu menu;
+
     private final String URL = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json";
     private final String PIC = "http://inthecheesefactory.com/uploads/source/glidepicasso/cover.jpg";
 
     @Override
     protected void initLayout() {
         addView(R.layout.activity_main);
+        initMenu();
     }
 
     @Override
@@ -46,7 +51,8 @@ public class MainActivity extends BaseActivity {
         switch (view.getId()) {
             case R.id.click:
 //                req();
-                pull();
+//                pull();
+                sliding();
 //                showDialog();
 //                showProgress(true);
                 break;
@@ -94,5 +100,38 @@ public class MainActivity extends BaseActivity {
                 mText.setText("error");
             }
         });
+    }
+
+    private void sliding() {
+        if (menu == null)
+            return;
+        menu.setSlidingEnabled(true);
+        menu.toggle();
+    }
+
+    private void initMenu() {
+        if (menu == null) {
+            menu = new SlidingMenu(this);
+            menu.setMode(SlidingMenu.LEFT);
+            menu.setSlidingEnabled(false);
+            menu.setTouchModeBehind(SlidingMenu.TOUCHMODE_FULLSCREEN);
+            menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+            menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+            menu.setMenu(R.layout.layout_home_sliding);
+            menu.setOffsetFadeDegree(0.25f);
+            menu.setFadeDegree(0.35f);
+            menu.setShadowWidth(15);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (menu != null && menu.isMenuShowing()) {
+                menu.toggle();
+                return true;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
