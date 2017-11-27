bcbe86069c700c13cfd164dbb6212a09f223d6a5
wtghby <wtghbyhui1@163.com>
2017-11-28 00:51:10 +0800

    添加服务器地址

--- app/src/main/java/com/conny/frame/api/ApiManager.java
+++ app/src/main/java/com/conny/frame/api/ApiManager.java
@@ -10,11 +10,12 @@ public class ApiManager {
     //正式发版时需要置为true
     public static final boolean RELEASE = false;
 
-//    private static final String API_DEVELOP_SERVER = "http://int.dpool.sina.com.cn/";
-private static final String API_DEVELOP_SERVER = "http://192.168.0.101:8080/";
+    //    private static final String API_DEVELOP_SERVER = "http://int.dpool.sina.com.cn/";
+    private static final String API_DEVELOP_SERVER = "http://192.168.0.101:8080/";
     private static final String API_TEST_SERVER = "";
-    private static final String API_CLUSTER_SERVER = "";
+    private static final String API_CLUSTER_SERVER = "http://47.93.10.147:8080/tv/";
 
-    public static String SERVER = API_DEVELOP_SERVER;
+    public static String SERVER = API_CLUSTER_SERVER;
+//    public static String SERVER = API_DEVELOP_SERVER;
 
 }
